f9878e2e4df72322441967a80b5a1ad49fe7be44
Krystof Rainisch <krystof.rainisch@hey.com>
2021-04-20 10:15:48 +0000

    change script

--- bin/addUser.sh
+++ bin/addUser.sh
@@ -2,4 +2,4 @@
 email=$1
 username=$2
 password=$3
-curl -s 'http://localhost:3030/users/'  -H 'Content-Type: application/json' --data-binary "{ \"email\": \"$email\", \"password\": \"$password\", \"username\": \"$username\" }" | jq .
+curl -s 'http://localhost:3030/users/'  -H 'Content-Type: application/json' --data-binary "{ \"email\": \"$email\", \"password\": \"$password\", \"username\": \"$username\" }"

e4da3a856777ed4c528f33ca523aa0385f2f7a35
Krystof Rainisch <krystof.rainisch@hey.com>
2021-04-26 14:31:32 +0200

    add products and instances tests

--- test/services/instances.test.ts
+++ test/services/instances.test.ts
@@ -1,8 +1,67 @@
 import app from '../../src/app';
+// import { noop } from '../../utils/helpers';
 
 describe('\'instances\' service', () => {
-  it('registered the service', () => {
+  it('registered the instance', () => {
     const service = app.service('instances');
     expect(service).toBeTruthy();
+  });
+
+  describe('create instance', () => {
+    const userInfo = {
+      email: 'test-instance-hooks@example.com',
+      password: 'supersecret',
+      username: 'test-instance-hooks',
+    };
+    let user: any;
+    let group: any;
+    let instance: any;
+    let product: any;
+    let space: any;
+
+    beforeAll(async () => {
+      user = await app.service('users').create(userInfo);
+      product = await app.service('products').create({
+        name: 'instance-test product',
+      });
+      group = await app.service('groups').create({
+        owner: user._id,
+        name: 'instance-test group',
+      });
+      space = await app.service('spaces').create({
+        name: 'instance-test space',
+        group: group._id
+      });
+    });
+
+    it('creates the instance', async () => {
+      const purchaseDate = new Date();
+      const untilDate = new Date();
+      const quantity = 1;
+      instance = await app.service('instances').create({
+        product: product._id,
+        space: space._id,
+        quantity,
+        purchaseDate,
+        untilDate,
+      });
+
+      expect(instance).toBeTruthy();
+      expect(instance.space._id).toEqual(space._id);
+      expect(instance.product._id).toEqual(product._id);
+      expect(instance.quantity).toEqual(quantity);
+      expect(instance.purchaseDate).toEqual(purchaseDate);
+      expect(instance.untilDate).toEqual(untilDate);
+    });
+
+    it('removes the instance', async () => {
+      await app.service('instances').remove(instance._id, {
+        user,
+      });
+
+      const deletedInstance = app.service('instances').get(instance._id);
+
+      expect(deletedInstance).rejects.toBeInstanceOf(Error);
+    });
   });
 });
--- test/services/products.test.ts
+++ test/services/products.test.ts
@@ -1,8 +1,44 @@
 import app from '../../src/app';
+// import { noop } from '../../utils/helpers';
 
 describe('\'products\' service', () => {
-  it('registered the service', () => {
+  it('registered the product', () => {
     const service = app.service('products');
     expect(service).toBeTruthy();
+  });
+
+  describe('create product', () => {
+    const userInfo = {
+      email: 'test-product-hooks@example.com',
+      password: 'supersecret',
+      username: 'test-product-hooks',
+    };
+    let user: any;
+    let product: any;
+
+    beforeAll(async () => {
+      user = await app.service('users').create(userInfo);
+    });
+
+    it('creates the product', async () => {
+      product = await app.service('products').create({
+        name: 'test product',
+      });
+
+      expect(product).toBeTruthy();
+      expect(product.name).toEqual('test product');
+      expect(product.unit).toEqual('UNIT');
+      expect(product.defaultQuantity).toEqual(1);
+    });
+
+    it('removes the product', async () => {
+      await app.service('products').remove(product._id, {
+        user,
+      });
+
+      const deletedProduct = app.service('products').get(product._id);
+
+      expect(deletedProduct).rejects.toBeInstanceOf(Error);
+    });
   });
 });
